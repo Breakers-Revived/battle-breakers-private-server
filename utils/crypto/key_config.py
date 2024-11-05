3e291ecc8d6f10a110198a7f6ea10cf0e658807a
Chaikew <68963282+Chaikew@users.noreply.github.com>
2024-11-05 10:20:31 +0000

    Added header for new py file

--- utils/crypto/key_config.py
+++ utils/crypto/key_config.py
@@ -1,3 +1,11 @@
+"""
+Battle Breakers Private Server / Master Control Program ""Emulator"" Copyright 2024 by Alexander Hanson (Dippyshere).
+Please do not skid my hard work.
+https://github.com/dippyshere/battle-breakers-private-server
+This code is licensed under the Breakers Revived License (BRL).
+
+This file contains PEM key files config
+"""
 from os import getcwd, path
 
 __location__ = path.realpath(path.join(getcwd(), path.dirname(__file__)))
