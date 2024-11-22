1b3bf24085e4a628569e522fd7dea41878fed029
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2024-11-22 21:28:58 +1100

    fix for database saving on graceful shutdown
    Signed-off-by: Alex Hanson <43489279+dippyshere@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Please do not skid my hard work.
 https://github.com/dippyshere/battle-breakers-private-server
 This code is licensed under the Breakers Revived License (BRL).
 """
+import os
+
 from typing_extensions import Any, Type
 
 from api import api
@@ -43,34 +45,41 @@ app.ctx.invalid_tokens = []
 
 
 @app.before_server_start
-async def attach_db(*_: Any) -> None:
+async def attach_db(_app: sanic.app.Sanic[TomlConfig, Type[types.Context]], *_) -> None:
     """
     Called when the server is started
-    :param _: The loop
+    :param _app: The app
     """
-    app.ctx.db = motor.motor_asyncio.AsyncIOMotorClient(app.config.DATABASE["URI"])[app.config.DATABASE["DATABASE"]]
-    app.ctx.db.client.timeoutMS = 1000
-    app.ctx.db.client.socketTimeoutMS = 1000
-    app.ctx.db.client.connectTimeoutMS = 1000
-    app.ctx.db.client.serverSelectionTimeoutMS = 1500
-    app.ctx.calendar = await ScheduledEvents.init_calendar()
-    app.ctx.storefronts = await StoreCatalogue.init_storefront()
-    app.ctx.lightswitch = await LightswitchService.init_lightswitch(app.ctx.db)
+    _app.ctx.db = motor.motor_asyncio.AsyncIOMotorClient(_app.config.DATABASE["URI"])[_app.config.DATABASE["DATABASE"]]
+    _app.ctx.db.client.timeoutMS = 1000
+    _app.ctx.db.client.socketTimeoutMS = 1000
+    _app.ctx.db.client.connectTimeoutMS = 1000
+    _app.ctx.db.client.serverSelectionTimeoutMS = 1500
+    _app.ctx.calendar = await ScheduledEvents.init_calendar()
+    _app.ctx.storefronts = await StoreCatalogue.init_storefront()
+    _app.ctx.lightswitch = await LightswitchService.init_lightswitch(_app.ctx.db)
 
 
-@app.before_server_stop
-async def server_stop(*_: Any) -> None:
+@app.after_server_stop
+async def server_stop(_app: sanic.app.Sanic[TomlConfig, Type[types.Context]], *_) -> None:
     """
     Called when the server is stopped
-    :param _: The loop
+    :param _app: The app
     """
-    for profile in app.ctx.profiles.values():
+    print("Server stopped")
+    print("Flushing changes and saving profiles...")
+    for profile in _app.ctx.profiles.values():
         await profile.flush_changes()
         await profile.save_profile()
-    for profile in app.ctx.friends.values():
+    for profile in _app.ctx.friends.values():
         await profile.save_friends()
-    await app.ctx.lightswitch.save_lightswitch()
+    await _app.ctx.lightswitch.save_lightswitch()
 
 
 if __name__ == "__main__":
-    app.run(host=app.config.SERVER["HOST"], port=app.config.SERVER["PORT"], auto_reload=True, access_log=False)
+    # workaround for sanic bug on windows where after_server_stop is not called when running with workers
+    if os.name == "nt":
+        app.run(host=app.config.SERVER["HOST"], port=app.config.SERVER["PORT"], single_process=True)
+        # app.run(host=app.config.SERVER["HOST"], port=app.config.SERVER["PORT"], dev=True, debug=True, motd=False)
+    else:
+        app.run(host=app.config.SERVER["HOST"], port=app.config.SERVER["PORT"], fast=True)
